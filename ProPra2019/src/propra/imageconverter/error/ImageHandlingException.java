b3a39fd7bdb9fbcbf48e1878e1f8fb1586f23327
Oliver <Oliver@DESKTOP-M522RF7.fritz.box>
2020-01-03 20:07:21 +0100

    Final commitment.

--- ProPra2019/src/propra/imageconverter/error/ImageHandlingException.java
+++ ProPra2019/src/propra/imageconverter/error/ImageHandlingException.java
@@ -1,8 +1,9 @@
 package propra.imageconverter.error;
 
 /**
- * An <code>ImageHandlingException</code> is thrown when an error occurred
- * during the use of the <code>ImageConverter</code>.
+ * An <code>ImageHandlingException</code> extends the class
+ * <code>Exception</code> and is thrown when an error occurred during the use of
+ * the <code>ImageConverter</code>.
  * 
  * @author Oliver Eckstein
  *
@@ -13,6 +14,12 @@ public class ImageHandlingException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates a new <code>ImageHandlingException</code>.
+	 * 
+	 * @param msg the exception message
+	 * @param errorCode the error code
+	 */
 	public ImageHandlingException(String msg, ImageConverterErrorCode errorCode) {
 		this.errorCode = errorCode.getErrorCode();
 		System.err.println(msg + " Error code: " + errorCode.getErrorCode());
